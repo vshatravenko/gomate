2514b5cb79349b3c628a7d618dcfcadfdbf58dd6
vshatravenko <vshatravenko@heliostech.fr>
2019-04-06 22:22:19 +0300

    Add test cases for rich types

--- pkg/storage/storage_test.go
+++ pkg/storage/storage_test.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"bytes"
+	"encoding/json"
 	"os"
 	"testing"
 )
@@ -66,5 +68,76 @@ func TestBasic(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	os.Remove("storage-test.db")
+}
+
+type BasicStruct struct {
+	Content []string
+}
+
+func TestTypes(t *testing.T) {
+	var input1 = map[string]string{
+		"Pink Floyd": "Time",
+		"The Cure":   "Faith",
+		"Deftones":   "MX",
+		"Placebo":    "Lady of the Flowers",
+	}
+
+	output1 := make(map[string]string)
+
+	testGetPut(t, input1, &output1)
+
+	var input2 = BasicStruct{
+		[]string{"there's", "more", "things", "to", "life"},
+	}
+
+	output2 := BasicStruct{}
+
+	testGetPut(t, input2, &output2)
+}
+
+func testGetPut(t *testing.T, inVal interface{}, outVal interface{}) {
+	// Clean up the test storage file
+	os.Remove("storage-test.db")
+
+	// Open the storage
+	db, err := Open("storage-test.db")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Marshal the inVal
+	input, err := json.Marshal(inVal)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Put inVal into the storage
+	if err := db.Put("nyess", inVal); err != nil {
+		t.Fatal(err)
+	}
+
+	// Get it out
+	if err := db.Get("nyess", outVal); err != nil {
+		t.Fatal(err)
+	}
+
+	// Marshal the outVal
+	output, err := json.Marshal(outVal)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Check if the inVal and outVal are equal
+	if !bytes.Equal(input, output) {
+		t.Fatal("input and output are not equal")
+	}
+
+	// Close the db
+	if err := db.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	// Clean up the test storage file
 	os.Remove("storage-test.db")
 }
